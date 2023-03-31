a764233ebf53c1792f7d465777755a1aea4da4f1
Niceplugin <niceplugin@gmail.com>
2023-03-31 11:48:34 +0900

    pwa-test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,42 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.png'],
+      manifest: {
+        "name": "Web Code Playground",
+        "short_name": "Playground",
+        "icons": [
+          {
+            "src": "/pwa/icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/pwa/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ],
+        "start_url": "/index.html",
+        "display": "standalone",
+        "theme_color": "#282c34",
+        "background_color": "#282c34"
+      }
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
